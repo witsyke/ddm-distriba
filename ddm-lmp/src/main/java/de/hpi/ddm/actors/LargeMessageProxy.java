a3654a527af4237eca275f9ca9302989c1bf5030
witsyke <witzke.simon@gmail.com>
2019-11-12 22:56:48 +0100

    Creates functioning version with akka streams and fixes tests to work with akka 2.6.0

--- ddm-lmp/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
+++ ddm-lmp/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
@@ -1,82 +1,91 @@
 package de.hpi.ddm.actors;
 
-import java.io.Serializable;
+import java.io.*;
 
-import akka.actor.AbstractLoggingActor;
-import akka.actor.ActorRef;
-import akka.actor.ActorSelection;
-import akka.actor.Props;
+import akka.actor.*;
+import akka.stream.Materializer;
+import akka.stream.SourceRef;
+import akka.stream.StreamRefResolver;
+import akka.stream.impl.streamref.StreamRefResolverImpl;
+import akka.stream.javadsl.*;
+import akka.util.ByteString;
+import akka.util.ByteStringBuilder;
+import de.hpi.ddm.structures.KryoPoolSingleton;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 public class LargeMessageProxy extends AbstractLoggingActor {
+    ////////////////////////
+    // Actor Construction //
+    ////////////////////////
+    public static final String DEFAULT_NAME = "largeMessageProxy";
 
-	////////////////////////
-	// Actor Construction //
-	////////////////////////
+    public static Props props() {
+        return Props.create(LargeMessageProxy.class);
+    }
 
-	public static final String DEFAULT_NAME = "largeMessageProxy";
-	
-	public static Props props() {
-		return Props.create(LargeMessageProxy.class);
-	}
+    ////////////////////
+    // Actor Messages //
+    ////////////////////
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class LargeMessage<T> implements Serializable {
+        private static final long serialVersionUID = 5257188484780972570L;
+        private T message;
+        private ActorRef receiver;
+    }
 
-	////////////////////
-	// Actor Messages //
-	////////////////////
-	
-	@Data @NoArgsConstructor @AllArgsConstructor
-	public static class LargeMessage<T> implements Serializable {
-		private static final long serialVersionUID = 2940665245810221108L;
-		private T message;
-		private ActorRef receiver;
-	}
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class SourceMessage implements Serializable {
+        private static final long serialVersionUID = 3647485967346346574L;
+        private String sourceRef;
+        private ActorRef sender;
+        private ActorRef receiver;
+    }
 
-	@Data @NoArgsConstructor @AllArgsConstructor
-	public static class BytesMessage<T> implements Serializable {
-		private static final long serialVersionUID = 4057807743872319842L;
-		private T bytes;
-		private ActorRef sender;
-		private ActorRef receiver;
-	}
-	
-	/////////////////
-	// Actor State //
-	/////////////////
-	
-	/////////////////////
-	// Actor Lifecycle //
-	/////////////////////
+    /////////////////
+    // Actor State //
+    /////////////////
+    private Materializer materializer = Materializer.createMaterializer(this.context());
+    private StreamRefResolver streamRefResolver = new StreamRefResolverImpl((ExtendedActorSystem) this.context().system());
 
-	////////////////////
-	// Actor Behavior //
-	////////////////////
-	
-	@Override
-	public Receive createReceive() {
-		return receiveBuilder()
-				.match(LargeMessage.class, this::handle)
-				.match(BytesMessage.class, this::handle)
-				.matchAny(object -> this.log().info("Received unknown message: \"{}\"", object.toString()))
-				.build();
-	}
+    /////////////////////
+    // Actor Lifecycle //
+    /////////////////////
+    ////////////////////
+    // Actor Behavior //
+    ////////////////////
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .match(LargeMessage.class, this::handle)
+                .match(SourceMessage.class, this::handle)
+                .matchAny(object -> this.log().info("Received unknown message: \"{}\"", object.toString()))
+                .build();
+    }
 
-	private void handle(LargeMessage<?> message) {
-		ActorRef receiver = message.getReceiver();
-		ActorSelection receiverProxy = this.context().actorSelection(receiver.path().child(DEFAULT_NAME));
-		
-		// This will definitely fail in a distributed setting if the serialized message is large!
-		// Solution options:
-		// 1. Serialize the object and send its bytes batch-wise (make sure to use artery's side channel then).
-		// 2. Serialize the object and send its bytes via Akka streaming.
-		// 3. Send the object via Akka's http client-server component.
-		// 4. Other ideas ...
-		receiverProxy.tell(new BytesMessage<>(message.getMessage(), this.sender(), message.getReceiver()), this.self());
-	}
+    private void handle(LargeMessage<?> message) {
+        ActorRef receiver = message.getReceiver();
+        ActorSelection receiverProxy = this.context().actorSelection(receiver.path().child(DEFAULT_NAME));
 
-	private void handle(BytesMessage<?> message) {
-		// Reassemble the message content, deserialize it and/or load the content from some local location before forwarding its content.
-		message.getReceiver().tell(message.getBytes(), message.getSender());
-	}
+        SourceRef<ByteString> sourceRef = StreamConverters.fromInputStream(() ->
+                new ByteArrayInputStream(KryoPoolSingleton.get().toBytesWithClass(message.getMessage())), 512 ^ 2)
+                .runWith(StreamRefs.sourceRef(), materializer);
+
+        receiverProxy.tell(new SourceMessage(streamRefResolver.toSerializationFormat(sourceRef), this.sender(), message.getReceiver()), this.self());
+    }
+
+    private void handle(SourceMessage message) {
+        SourceRef<ByteString> objectSourceRef = streamRefResolver.resolveSourceRef(message.sourceRef);
+
+        ByteStringBuilder builder = ByteString.createBuilder();
+        objectSourceRef.getSource().runWith(StreamConverters.asJavaStream(), materializer)
+                .forEach(builder::append);
+
+        message.getReceiver().tell(KryoPoolSingleton.get().fromBytes(builder.result().toArray()), message.getSender());
+    }
 }
