cd9f9619de2b43e116d9f80291a086eeb0648a01
chrizzzlybear <chris.warmuth@t-online.de>
2020-01-11 18:12:55 +0100

    Creates a seperate folder for IDD

---
+++ idd/src/main/scala/de/hpi/idd/Sindy.scala
@@ -0,0 +1,11 @@
+package de.hpi.idd
+
+import org.apache.spark.sql.{Dataset, Row, SparkSession}
+
+object Sindy {
+
+  def discoverINDs(inputs: List[String], spark: SparkSession): Unit = {
+
+    // TODO
+  }
+}
