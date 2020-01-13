66cf973687c7fa7b4141a837a26a1a0d860d06bb
witsyke <witzke.simon@gmail.com>
2020-01-13 21:44:13 +0100

    Updates Readme.md

--- idd/Readme.md
+++ idd/Readme.md
@@ -1,7 +1,11 @@
-This is a basic scala project that includes spark dependencies and necessary build-configuration to build a jar that can be submitted to a spark installation via spark-submit.
-Things to do after checking out and opening in intellij:
-    Add scala SDK 2.11.11 (tested with this version, it has to be 2.11.x since this is the spark version)
-    Add jdk 8
-    add Framework support of Maven
-To build the project:
-    run mvn package
+# DDM - IDD
+
+This is a scala project that solves the Inclusion Dependency Discovery exercises for 
+the Distributed Data Management lecture @ HPI in the winter semester 2019/20.
+
+The solution builds a simple Spark pipeline that is structured after the SINDY algorithm proposed by Sebastian Kruse, Thorsten Papenbrock and Felix Naumann in
+their 2015 paper: Scaling Out the Discovery of Inclusion Dependencies (https://hpi.de/fileadmin/user_upload/fachgebiete/naumann/publications/2015/Scaling_out_the_discovery_of_INDs-CR.pdf)
+
+### Performance
+On the local Windows machine with 4 cores, where the application was tested, the execution time to find
+all IDD's in the TPCH dataset ("nation", "region", "supplier", "customer", "part", "lineitem", "orders") was ~ 45 seconds.
--- idd/src/main/scala/de/hpi/idd/Idd.scala
+++ idd/src/main/scala/de/hpi/idd/Idd.scala
@@ -1,11 +1,6 @@
 package de.hpi.idd
 
-import org.apache.spark.sql.{Dataset, Encoder, SparkSession}
-import org.apache.spark.ml.Pipeline
-import org.apache.spark.ml.classification.DecisionTreeClassificationModel
-import org.apache.spark.ml.classification.DecisionTreeClassifier
-import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
-import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorIndexer}
+import org.apache.spark.sql.SparkSession
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
 
@@ -67,18 +62,14 @@ object Idd extends App {
     //------------------------------------------------------------------------------------------------------------------
     // Reading the files
     //------------------------------------------------------------------------------------------------------------------
-
-    val inputs = List("nation", "region", "supplier", "customer", "part", "lineitem", "orders")
+    val inputs = List("nation", "region", "supplier", "customer", "part", "lineitem", "orders") // TODO work on input -> it should take all files in folder not just the ones with these names
       .map(name => s"data/TPCH/tpch_$name.csv")
 
     //------------------------------------------------------------------------------------------------------------------
     // Execute IND-Discovery
     //------------------------------------------------------------------------------------------------------------------
-    2
     time {
       Sindy.discoverINDs(inputs, spark)
     }
-
-
   }
 }
